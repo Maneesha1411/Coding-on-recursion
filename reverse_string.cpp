7929f5cb2e34bb947ea26ecb0ea87560faee934f
Maneesha1411 <guttamaneesha456@gmail.com>
2025-08-02 11:34:58 +0530

    both iterative and recursion

--- reverse_string.cpp
+++ reverse_string.cpp
@@ -24,9 +24,30 @@ bool palindrome(string s)
 {
     return palindrome(s, 0, s.size() - 1);
 }
+
+// using iterative approach
+void palindrom(string s, int left, int right)
+{
+    while (left < right)
+    {
+        while (left < right && !isalnum(s[left]))
+            left++;
+        while (left < right && !isalnum(s[right]))
+            right--;
+        if (tolower(s[left]) != tolower(s[right]))
+        {
+            cout << "\nNot a palindrome";
+            return;
+        }
+        left++;
+        right--;
+    }
+    cout << "\nispalindrome";
+}
 int main()
 {
     string s = "A man, a plan, a canal: Panama";
     palindrome(s) ? cout << "yes" : cout << "NO";
+    palindrom(s, 0, s.size() - 1);
     return 0;
 }
